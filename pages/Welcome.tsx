efc92519940bcac0c4f159d4f40087c25f70685c
tokyozen <77097819+tokyozen@users.noreply.github.com>
2025-09-23 14:35:06 +0100

    Updated Welcome.tsx

--- pages/Welcome.tsx
+++ pages/Welcome.tsx
@@ -16,7 +16,7 @@ const Welcome: React.FC = () => {
       <Card>
         <div className="mb-6">
           <img 
-            src="https://images.pexels.com/photos/1024993/pexels-photo-1024993.jpeg?auto=compress&cs=tinysrgb&w=600&h=400&fit=crop" 
+            src="https://i.imgur.com/3Mz11hN.jpeg?auto=compress&cs=tinysrgb&w=600&h=400&fit=crop" 
             alt="Albert and Isha" 
             className="w-64 h-64 object-cover rounded-full mx-auto shadow-lg border-4 border-[#cc5500]"
           />
